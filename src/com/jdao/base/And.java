563984e3539290ee3c52bde4b4f23bd6fa8e6845
donnie <donnie4w@gmail.com>
2017-12-06 12:03:37 +0800

    modified

--- src/com/jdao/base/And.java
+++ src/com/jdao/base/And.java
@@ -1,5 +1,12 @@
 package com.jdao.base;
 
+/**
+ * @File:jdao: com.jdao.base :And.java
+ * @Date:2017年12月6日
+ * @Copyright (c) 2017, donnie4w@gmail.com All Rights Reserved.
+ * @Author: dong
+ * @Desc:
+ */
 public class And {
 	public static Where[] and(Where... wheres) {
 		String f = wheres[0].getExpression();
@@ -10,5 +17,11 @@ public class And {
 			wheres[wheres.length - 1].setExpression(wheres[wheres.length - 1].getExpression() + ")");
 		}
 		return wheres;
+	}
+
+	public static Where and(Where where) {
+		String f = where.getExpression();
+		f = f.replaceFirst(" and ", " and (");
+		return where.setExpression(f + ")");
 	}
 }
