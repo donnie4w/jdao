bc2e6bd5b40010754766dde603a39f6256d45806
donnie4w <donnie4w@gmail.com>
2024-07-01 19:36:17 +0800

    Update Array.java

--- src/main/java/io/github/donnie4w/jdao/base/Array.java
+++ src/main/java/io/github/donnie4w/jdao/base/Array.java
@@ -1,23 +1,35 @@
-/**
- *  https://github.com/donnie4w/jdao
- *  Copyright jdao Author. All Rights Reserved.
- *  Email: donnie4w@gmail.com
+/*
+ * Copyright (c) 2024, donnie <donnie4w@gmail.com> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * github.com/donnie4w/jdao
  */
 package io.github.donnie4w.jdao.base;
 /**
- * Copyright 2012-2013 donnie(donnie4w@gmail.com)
- * date 2013-1-10
- * verion 1.0
+ * @Copyright 2012-2013 donnie(donnie4w@gmail.com)
+ * @date 2013-1-10
+ * @verion 1.0
  */
 public class Array {
-	Object[] array;
+    Object[] array;
 
-	public Array(Object... objects) {
-		array = objects;
-	}
+    public Array(Object... objects) {
+        array = objects;
+    }
 
-	public Object[] getArray() {
-		return array;
-	}
+    public Object[] getArray() {
+        return array;
+    }
 
 }
