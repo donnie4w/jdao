dc760d8b143d928d728fa3acbd5d3384bdab2c84
donnie4w <donnie4w@gmail.com>
2024-07-01 19:37:10 +0800

    Update InOut.java

--- src/main/java/io/github/donnie4w/jdao/base/InOut.java
+++ src/main/java/io/github/donnie4w/jdao/base/InOut.java
@@ -1,29 +1,41 @@
-/**
- *  https://github.com/donnie4w/jdao
- *  Copyright jdao Author. All Rights Reserved.
- *  Email: donnie4w@gmail.com
+/*
+ * Copyright (c) 2024, donnie <donnie4w@gmail.com> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * github.com/donnie4w/jdao
  */
 package io.github.donnie4w.jdao.base;
 
 /**
- * Copyright 2012-2013 donnie(donnie4w@gmail.com)
- * date 2013-2-2
- * verion 1.0.1 出入参
+ * @Copyright 2012-2013 donnie(donnie4w@gmail.com)
+ * @date 2013-2-2
+ * @verion 1.0.1 出入参
  */
 public class InOut extends Params {
-	private Object o;
-	private Type t;
+    private Object o;
+    private Type t;
 
-	public InOut(Object o, Type t) {
-		this.o = o;
-		this.t = t;
-	}
+    public InOut(Object o, Type t) {
+        this.o = o;
+        this.t = t;
+    }
 
-	public Object getValue() {
-		return o;
-	}
+    public Object getValue() {
+        return o;
+    }
 
-	public int getTypes() {
-		return Out.types2java(t);
-	}
+    public int getTypes() {
+        return Out.types2java(t);
+    }
 }
