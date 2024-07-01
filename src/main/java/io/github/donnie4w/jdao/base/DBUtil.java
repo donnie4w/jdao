74af90edf46bb6a2ec64bd1e8d8260a3eddd5731
donnie4w <donnie4w@gmail.com>
2024-07-01 19:36:43 +0800

    Delete DBUtil.java

--- src/main/java/io/github/donnie4w/jdao/base/DBUtil.java
+++
@@ -1,9 +0,0 @@
-/**
- * https://github.com/donnie4w/jdao
- * Copyright jdao Author. All Rights Reserved.
- * Email: donnie4w@gmail.com
- */
-package io.github.donnie4w.jdao.base;
-
-public class DBUtil extends DBUtils<DBUtil>{
-}
