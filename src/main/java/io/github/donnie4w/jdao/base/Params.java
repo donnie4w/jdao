f45562f5aa1f02a76536874254198bc4a818ea21
donnie4w <donnie4w@gmail.com>
2024-07-02 19:54:53 +0800

    Update Params.java

--- src/main/java/io/github/donnie4w/jdao/base/Params.java
+++ src/main/java/io/github/donnie4w/jdao/base/Params.java
@@ -1,31 +1,43 @@
-/**
- *  https://github.com/donnie4w/jdao
- *  Copyright jdao Author. All Rights Reserved.
- *  Email: donnie4w@gmail.com
+/*
+ * Copyright (c) 2024, donnie <donnie4w@gmail.com> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * github.com/donnie4w/jdao
  */
 package io.github.donnie4w.jdao.base;
 
 /**
- * Copyright 2012-2013 donnie(donnie4w@gmail.com)
- * date 2013-2-2
- * verion 1.0.1
+ * @Copyright 2012-2013 donnie(donnie4w@gmail.com)
+ * @date 2013-2-2
+ * @verion 1.0.1
  */
 public abstract class Params {
 
-	public static In IN(Object o) {
-		return new In(o);
-	}
+    public static In IN(Object o) {
+        return new In(o);
+    }
 
-	public static Out OUT(Type t) {
-		return new Out(t);
-	}
+    public static Out OUT(Type t) {
+        return new Out(t);
+    }
 
-	public static InOut INOUT(Object o, Type t) {
-		return new InOut(o, t);
-	}
+    public static InOut INOUT(Object o, Type t) {
+        return new InOut(o, t);
+    }
 
-	public abstract Object getValue();
+    public abstract Object getValue();
 
-	public abstract int getTypes();
+    public abstract int getTypes();
 
 }
