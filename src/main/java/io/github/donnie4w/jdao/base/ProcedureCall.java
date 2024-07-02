7b86fde3251d5f1fcc4ea9daea50654288a4ef1d
donnie4w <donnie4w@gmail.com>
2024-07-02 19:54:56 +0800

    Update ProcedureCall.java

--- src/main/java/io/github/donnie4w/jdao/base/ProcedureCall.java
+++ src/main/java/io/github/donnie4w/jdao/base/ProcedureCall.java
@@ -1,122 +1,128 @@
-/**
- *  https://github.com/donnie4w/jdao
- *  Copyright jdao Author. All Rights Reserved.
- *  Email: donnie4w@gmail.com
+/*
+ * Copyright (c) 2024, donnie <donnie4w@gmail.com> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * github.com/donnie4w/jdao
  */
 package io.github.donnie4w.jdao.base;
 
+import io.github.donnie4w.jdao.dbHandler.DBType;
+import io.github.donnie4w.jdao.dbHandler.DBhandle;
+import io.github.donnie4w.jdao.dbHandler.JdaoException;
+
+import javax.sql.DataSource;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
-import io.github.donnie4w.jdao.util.Utils;
-
 /**
- * Copyright 2012-2013 donnie(donnie4w@gmail.com)
- * date 2013-2-2
- * verion 1.0.1 存储过程调用
+ * @Copyright 2012-2013 donnie(donnie4w@gmail.com)
+ * @date 2013-2-2
+ * @verion 1.0.1 存储过程调用
  */
 public class ProcedureCall {
 
-	private String sql;
-	private Params[] params;
+    private  String procedureName;
+    private  Params[] params;
 
-	private Map<Integer, Object> valuesMap = new HashMap<Integer, Object>();
-	private Map<Integer, Object> inTypesMap = new HashMap<Integer, Object>();
-	private Map<Integer, Integer> outTypesMap = new HashMap<Integer, Integer>();
+    private final Map<Integer, Object> valuesMap = new HashMap<Integer, Object>();
+    private final Map<Integer, Object> inTypesMap = new HashMap<Integer, Object>();
+    private final Map<Integer, Integer> outTypesMap = new HashMap<Integer, Integer>();
+    private String placeholderStr = "";
 
-	public ProcedureCall(Connection conn, String sql, Params... params) throws JException {
-		this.sql = sql;
-		this.params = params;
-		parseTypes();
-		call_(conn);
-	}
+    public ProcedureCall(DataSource dataSource, DBType dbType, String procedureName, Params... params) throws JdaoException {
+        this.procedureName = procedureName;
+        this.params = params;
+        parseTypes();
+        call(Jdao.newDBhandle(dataSource,dbType));
+    }
 
-	public ProcedureCall(JdaoHandler jdao, String sql, Params... params) throws JException {
-		this.sql = sql;
-		this.params = params;
-		parseTypes();
-		call(jdao);
-	}
+    public ProcedureCall(String sql, Params... params) throws JdaoException {
+        this.procedureName = sql;
+        this.params = params;
+        parseTypes();
+        call(Jdao.getDefaultDBhandle());
+    }
 
-	public ProcedureCall(DataSource ds, String sql, Params... params) throws JException {
-		this.sql = sql;
-		this.params = params;
-		parseTypes();
-		call(JdaoHandlerFactory.getJdaoHandler(ds));
-	}
+    public Object value(int index) {
+        return valuesMap.get(index);
+    }
 
-	public ProcedureCall(String sql, Params... params) throws JException {
-		this.sql = sql;
-		this.params = params;
-		parseTypes();
-		call(DaoFactory.getJdaoHandler(ProcedureCall.class, null));
-	}
+    final void call(DBhandle dBhandle) throws JdaoException {
+        try (Connection con = dBhandle.getJdbcHandle().getDataSource().getConnection()) {
+            parseParams(dBhandle.getDBType());
+            call_(con);
+        } catch (SQLException e) {
+            throw new JdaoException(e);
+        }
+    }
 
-	public Object fieldIndex(int i) {
-		return valuesMap.get(i);
-	}
+    private void parseParams(DBType dbType){
+        int i = 1;
+        int length = params.length;
+        StringBuilder sb = new StringBuilder();
+        while (i <= length) {
+            if (dbType==DBType.POSTGRESQL){
+                sb.append("$").append(i);
+            }else{
+                sb.append("?");
+            }
+            if (i < length) {
+                sb.append(",");
+            }
+            i+=1;
+        }
+        placeholderStr = sb.toString();
+    }
 
-	final void call(JdaoHandler jdao) throws JException {
-		Connection con = null;
-		try {
-			con = jdao.getConnection();
-			call_(con);
-		} finally {
-			jdao.close(con);
-		}
-	}
+    final void call_(Connection conn) throws JdaoException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{call ").append(procedureName).append("(");
+        if (params != null) {
+            sb.append(placeholderStr);
+        }
+        sb.append(")}");
+        try (CallableStatement stmt = conn.prepareCall(sb.toString())) {
+            for (Integer i : inTypesMap.keySet()) {
+                stmt.setObject(i, inTypesMap.get(i));
+            }
+            for (Integer i : outTypesMap.keySet()) {
+                stmt.registerOutParameter(i, outTypesMap.get(i));
+            }
+            stmt.execute();
+            for (Integer i : outTypesMap.keySet()) {
+                valuesMap.put(i, stmt.getObject(i));
+            }
+        } catch (Exception e) {
+            throw new JdaoException(e);
+        }
+    }
 
-	final void call_(Connection conn) throws JException {
-		CallableStatement stmt = null;
-		try {
-			StringBuilder sb = new StringBuilder();
-			sb.append("{call ").append(sql).append("(");
-			if (params != null) {
-				int i = 0;
-				int length = params.length;
-				while (length-- > 0) {
-					sb.append("?");
-					if (i < length) {
-						sb.append(",");
-					}
-				}
-			}
-			sb.append(")}");
-			System.out.println(sb.toString());
-			stmt = conn.prepareCall(sb.toString());
-			for (Integer i : inTypesMap.keySet()) {
-				stmt.setObject(i, inTypesMap.get(i));
-			}
-			for (Integer i : outTypesMap.keySet()) {
-				stmt.registerOutParameter(i, outTypesMap.get(i));
-			}
-			stmt.execute();
-			for (Integer i : outTypesMap.keySet()) {
-				valuesMap.put(i, stmt.getObject(i));
-			}
-		} catch (Exception e){
-			throw new JException(e);
-		}finally {
-			Utils.close(stmt);
-		}
-	}
-
-	final void parseTypes() {
-		if (params != null) {
-			for (int i = 0; i < params.length; i++) {
-				if (params[i] instanceof Out) {
-					outTypesMap.put(i + 1, params[i].getTypes());
-				} else if (params[i] instanceof InOut) {
-					outTypesMap.put(i + 1, params[i].getTypes());
-					inTypesMap.put(i + 1, params[i].getValue());
-				} else {
-					inTypesMap.put(i + 1, params[i].getValue());
-				}
-			}
-		}
-	}
+    final void parseTypes() {
+        if (params != null) {
+            for (int i = 0; i < params.length; i++) {
+                if (params[i] instanceof Out) {
+                    outTypesMap.put(i + 1, params[i].getTypes());
+                } else if (params[i] instanceof InOut) {
+                    outTypesMap.put(i + 1, params[i].getTypes());
+                    inTypesMap.put(i + 1, params[i].getValue());
+                } else {
+                    inTypesMap.put(i + 1, params[i].getValue());
+                }
+            }
+        }
+    }
 }
