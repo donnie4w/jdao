48bfe25d4e2345ce2f4e1f0b64e79e4f7e613edb
donnie4w <donnie4w@gmail.com>
2024-07-17 14:34:46 +0800

    Update DBhandle.java

--- src/main/java/io/github/donnie4w/jdao/handle/DBhandle.java
+++ src/main/java/io/github/donnie4w/jdao/handle/DBhandle.java
@@ -1,45 +1,208 @@
 package io.github.donnie4w.jdao.handle;
 
-import io.github.donnie4w.jdao.base.Table;
-
 import java.util.List;
 
+/**
+ * author donnie4w <donnie4w@gmail.com>
+ * <p>
+ * DBhandle is the core interface for all sql operations
+ * and provides all Jdao operation functions
+ */
 public interface DBhandle {
-    public DBType getDBType();
 
-    public Transaction  getTransaction() throws JdaoException;;
+    /**
+     * return DB type e.g. MYSQL,  POSTGRESQL,  MARIADB,  SQLITE,  ORACLE,  SQLSERVER,  DB2
+     *
+     * @return
+     */
+    DBType getDBType();
 
-    public JdbcHandle getJdbcHandle();
+    /**
+     * return Transaction
+     *
+     * @return
+     * @throws JdaoException
+     */
+    Transaction getTransaction() throws JdaoException;
 
-    public <T> List<T> executeQueryScanList(Transaction transaction, Class<T> clz, String sql, Object... values) throws JdaoException;
+    JdbcHandle getJdbcHandle();
 
-    public <T> List<T> executeQueryScanList(Class<T> clz, String sql, Object... values) throws JdaoException;
+//    /**
+//     * @param transaction
+//     * @param clz
+//     * @param sql
+//     * @param values
+//     * @return
+//     * @param <T>
+//     * @throws JdaoException
+//     */
+//    public <T> List<T> executeQueryScanList(Transaction transaction, Class<T> clz, String sql, Object... values) throws JdaoException;
+//
+//    /**
+//     * @param clz
+//     * @param sql
+//     * @param values
+//     * @return
+//     * @param <T>
+//     * @throws JdaoException
+//     */
+//    public <T> List<T> executeQueryScanList(Class<T> clz, String sql, Object... values) throws JdaoException;
+//
+//    /**
+//     *
+//     * @param transaction
+//     * @param clz
+//     * @param sql
+//     * @param values
+//     * @return
+//     * @param <T>
+//     * @throws JdaoException
+//     */
+//    public <T> T executeQueryScan(Transaction transaction, Class<T> clz, String sql, Object... values) throws JdaoException;
+//
+//    /**
+//     * @param clz
+//     * @param sql
+//     * @param values
+//     * @return
+//     * @param <T>
+//     * @throws JdaoException
+//     */
+//    public <T> T executeQueryScan(Class<T> clz, String sql, Object... values) throws JdaoException;
 
-    public <T> T executeQueryScan(Transaction transaction, Class<T> clz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery transactional operation and returns DataBean,
+     * the first if there are multiple query results
+     *
+     * @param transaction
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    DataBean executeQueryBean(Transaction transaction, String sql, Object... values) throws JdaoException;
 
-    public <T> T executeQueryScan(Class<T> clz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery Non-transactional operation and returns DataBean,
+     * the first if there are multiple query results
+     *
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    DataBean executeQueryBean(String sql, Object... values) throws JdaoException;
 
-    public DataBean executeQueryBean(Transaction transaction, String sql, Object... values) throws JdaoException;
+    /**
+     * @param transaction
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    List<DataBean> executeQueryBeans(Transaction transaction, String sql, Object... values) throws JdaoException;
 
-    public DataBean executeQueryBean(String sql, Object... values) throws JdaoException;
+    /**
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    List<DataBean> executeQueryBeans(String sql, Object... values) throws JdaoException;
 
-    public List<DataBean> executeQueryBeans(Transaction transaction, String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery transactional operation and returns multiple pieces of data,
+     * or all of them if there are multiple query results
+     *
+     * @param transaction
+     * @param claz
+     * @param sql
+     * @param values
+     * @param <T>
+     * @return
+     * @throws JdaoException
+     */
+    <T> List<T> executeQueryList(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException;
 
-    public List<DataBean> executeQueryBeans(String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery Non-transactional operation and returns multiple pieces of data,
+     * or all of them if there are multiple query results
+     *
+     * @param claz
+     * @param sql
+     * @param values
+     * @param <T>
+     * @return
+     * @throws JdaoException
+     */
+    <T> List<T> executeQueryList(Class<T> claz, String sql, Object... values) throws JdaoException;
 
-    public <T extends Table<?>> List<T> executeQueryList(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery Non-transactional operation and returns one piece of data,
+     * the first if there are multiple query results
+     *
+     * @param transaction
+     * @param claz
+     * @param sql
+     * @param values
+     * @param <T>
+     * @return
+     * @throws JdaoException
+     */
+    <T> T executeQuery(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException;
 
-    public <T extends Table<?>> List<T> executeQueryList(Class<T> claz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeQuery Non-transactional operation and returns one piece of data,
+     * the first if there are multiple query results
+     *
+     * @param claz
+     * @param sql
+     * @param values
+     * @param <T>
+     * @return
+     * @throws JdaoException
+     */
+    <T> T executeQuery(Class<T> claz, String sql, Object... values) throws JdaoException;
 
-    public <T extends Table<?>> T executeQuery(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeUpdate transactional operation
+     *
+     * @param transaction
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    int executeUpdate(Transaction transaction, String sql, Object... values) throws JdaoException;
 
-    public <T extends Table<?>> T executeQuery(Class<T> claz, String sql, Object... values) throws JdaoException;
+    /**
+     * executeUpdate Non-transactional operation
+     *
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    int executeUpdate(String sql, Object... values) throws JdaoException;
 
-    public int executeUpdate(Transaction transaction, String sql, Object... values) throws JdaoException;
+    /**
+     * Batch transactional operations
+     *
+     * @param transaction
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    int[] executeBatch(Transaction transaction, String sql, List<Object[]> values) throws JdaoException;
 
-    public int executeUpdate(String sql, Object... values) throws JdaoException;
-
-    public int[] executeBatch(Transaction transaction, String sql, List<Object[]> values) throws JdaoException;
-
-    public int[] executeBatch(String sql, List<Object[]> values) throws JdaoException;
+    /**
+     * Batch Non-transactional operations
+     *
+     * @param sql
+     * @param values
+     * @return
+     * @throws JdaoException
+     */
+    int[] executeBatch(String sql, List<Object[]> values) throws JdaoException;
 }
