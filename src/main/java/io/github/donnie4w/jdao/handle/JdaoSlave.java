68162f961b52bfd6c459b2e1b3e8783e4c0e5ae4
donnie4w <donnie4w@gmail.com>
2024-07-29 23:54:27 +0800

    Update JdaoSlave.java

--- src/main/java/io/github/donnie4w/jdao/handle/JdaoSlave.java
+++ src/main/java/io/github/donnie4w/jdao/handle/JdaoSlave.java
@@ -27,86 +27,268 @@ public class JdaoSlave {
     protected JdaoSlave() {
     }
 
-    private static final SlaveHandler defaultSlaveHandler = new SlaveHandler();
+    private static final SlaveHandle defaultSlaveHandle = new SlaveHandler();
 
     public static int size() {
-        return defaultSlaveHandler.size();
+        return defaultSlaveHandle.size();
     }
 
     /**
-     * bind slave datasource  for table dao package
+     * Binds the specified package to use the given data source and database type for all standard entity classes within the package.
      *
-     * @param packageName
-     * @param dataSource
-     * @param dbtype
+     * @param packageName The name of the package containing the entity classes to bind.
+     * @param dataSource The data source to use for database connections.
+     * @param dbtype The type of the database (e.g., DBType.MYSQL, DBType.POSTGRESQL).
+     *
+     *<p>Description:
+     *   This method sets up all entity classes within the specified package to use the provided data source and database type for database qurey operation.
+     *
+     *<p>Example:
+     *
+     *<p> Assuming "com.example.entities" is the package containing the entity classes
+     *<p> And "dataSource" is an instance of DataSource configured for the database connection
+     *<p> And "DBType.MYSQL" represents the type of the database<p>
+     *<blockquote><pre>
+     *   JdaoSlave.bindPackage("com.example.entities", dataSource, DBType.MYSQL);
+     *</pre></blockquote>
      */
     public static void bindPackage(String packageName, DataSource dataSource, DBType dbtype) {
-        defaultSlaveHandler.bindPackage(packageName, dataSource, dbtype);
+        defaultSlaveHandle.bindPackage(packageName, dataSource, dbtype);
     }
 
+    /**
+     * Binds the specified package to use the given DBhandle for all standard entity classes within the package.
+     *
+     * @param packageName The name of the package containing the entity classes to bind.
+     * @param dBhandle The DBhandle that encapsulates the CRUD operations and data source management.
+     *
+     *<p>Description:
+     *   This method sets up all entity classes within the specified package to use the provided DB handle for database qurey operation.
+     *
+     *<p>Example:
+     *<p>  Assuming "com.example.entities" is the package containing the entity classes
+     *<p>  And "dBhandle" is an instance of DBhandle configured for the database connection and CRUD operations
+     * <blockquote><pre>
+     *   JdaoSlave.bindPackage("com.example.entities", dBhandle);
+     *</pre></blockquote>
+     */
     public static void bindPackage(String packageName, DBhandle dBhandle) {
-        defaultSlaveHandler.bindPackage(packageName, dBhandle);
-    }
-
-    public static void bindClass(Class<? extends Table<?>> clz, DataSource dataSource, DBType dbtype) {
-        defaultSlaveHandler.bindClass(clz, dataSource, dbtype);
-    }
-
-    public static void bindClass(Class<? extends Table<?>> clz, DBhandle dBhandle) {
-        defaultSlaveHandler.bindClass(clz, dBhandle);
-    }
-
-    public static boolean bindMapper(String namespace, String id, DataSource dataSource, DBType dbtype) {
-        return defaultSlaveHandler.bindMapper(namespace, id, dataSource, dbtype);
-    }
-
-    public static boolean bindMapper(String namespace, String id, DBhandle dBhandle) {
-        return defaultSlaveHandler.bindMapper(namespace, id, dBhandle);
-    }
-
-    public static void unbindMapper(String namespace, String id) {
-        defaultSlaveHandler.unbindMapper(namespace, id);
-    }
-
-
-    public static boolean bindMapper(Class<?> clz, DBhandle dBhandle) {
-        return defaultSlaveHandler.bindMapper(clz, dBhandle);
-    }
-
-    public static boolean bindMapper(Class<?> clz, DataSource dataSource, DBType dbtype) {
-        return defaultSlaveHandler.bindMapper(clz, dataSource, dbtype);
-    }
-
-    public static void unbindMapper(Class<?> clz) {
-        defaultSlaveHandler.unbindMapper(clz);
-    }
-
-    public static boolean bindMapper(String namespace, DataSource dataSource, DBType dbtype) {
-        return defaultSlaveHandler.bindMapper(namespace, dataSource, dbtype);
-    }
-
-    public static boolean bindMapper(String namespace, DBhandle dBhandle) {
-        return defaultSlaveHandler.bindMapper(namespace, dBhandle);
-    }
-
-    public static void unbindMapper(String namespace) {
-        defaultSlaveHandler.unbindMapper(namespace);
-    }
-
-    public static void unbindClass(Class<? extends Table<?>> clz) {
-        defaultSlaveHandler.unbindClass(clz);
+        defaultSlaveHandle.bindPackage(packageName, dBhandle);
     }
 
     public static void unbindPackage(String packageName) {
-        defaultSlaveHandler.unbindPackage(packageName);
+        defaultSlaveHandle.unbindPackage(packageName);
+    }
+
+
+    /**
+     * Binds the specified standard entity class to use the given data source and database type.
+     *
+     * @param clz The class representing the entity to bind.
+     * @param dataSource The data source to use for database connections.
+     * @param dbtype The type of the database (e.g., DBType.MYSQL, DBType.POSTGRESQL).
+     *
+     *<p>Description:
+     *   This method sets up the specified entity class to use the provided data source and database type for database qurey operation.
+     *
+     *<p>Example:
+     *<p>  Assuming User is a class representing the user entity
+     *<p>  And "dataSource" is an instance of DataSource configured for the database connection
+     *<p>  And "DBType.MYSQL" represents the type of the database
+     *<blockquote><pre>
+     *   JdaoSlave.bindClass(User.class, dataSource, DBType.MYSQL);
+     *</pre></blockquote>
+     */
+    public static void bindClass(Class<? extends Table<?>> clz, DataSource dataSource, DBType dbtype) {
+        defaultSlaveHandle.bindClass(clz, dataSource, dbtype);
+    }
+
+    /**
+     * Binds the specified standard entity class to use the given DB handle for database interactions.
+     *
+     * @param clz The class representing the entity to bind.
+     * @param dBhandle The DB handle that encapsulates the CRUD operations and data source management.
+     *
+     *<p>Description:
+     *   This method sets up the specified entity class to use the provided DB handle for database qurey operation.
+     *
+     *<p>Example:
+     *<p> Assuming User is a class representing the user entity
+     *<p> And "dBhandle" is an instance of DBhandle configured for the database connection and CRUD operations
+     *<blockquote><pre>
+     *   JdaoSlave.bindClass(User.class, dBhandle);
+     *</pre></blockquote>
+     */
+    public static void bindClass(Class<? extends Table<?>> clz, DBhandle dBhandle) {
+        defaultSlaveHandle.bindClass(clz, dBhandle);
+    }
+
+    public static void unbindClass(Class<? extends Table<?>> clz) {
+        defaultSlaveHandle.unbindClass(clz);
+    }
+
+
+
+    /**
+     * Binds the specified XML mapping namespace and CRUD tag ID to use the given data source and database type for the specified CRUD operation.
+     *
+     * @param namespace The namespace in the XML mapping files that corresponds to the CRUD operations to bind.
+     * @param id The ID of the CRUD tag within the namespace to bind.
+     * @param dataSource The data source to use for database connections.
+     * @param dbtype The type of the database (e.g., DBType.MYSQL, DBType.POSTGRESQL).
+     *
+     * @return true if the binding was successful, false otherwise.
+     *
+     *<p> Description:
+     *   This method sets up the specified XML mapping namespace and CRUD tag ID to use the provided data source and database type for database qurey operation.
+     *
+     *<p> Example:
+     *<p> Assuming "com.example.mappers.users" is the namespace in the XML mapping files
+     *<p> And "getUserById" is the ID of the CRUD tag within the namespace
+     *<p> And "dataSource" is an instance of DataSource configured for the database connection
+     *<p> And "DBType.MYSQL" represents the type of the database
+     * <blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper("com.example.mappers.users", "getUserById", dataSource, DBType.MYSQL);
+     *</pre></blockquote>
+     */
+    public static boolean bindMapper(String namespace, String id, DataSource dataSource, DBType dbtype) {
+        return defaultSlaveHandle.bindMapper(namespace, id, dataSource, dbtype);
+    }
+
+    /**
+     * Binds the specified XML mapping namespace and CRUD tag ID to use the given DB handle for the specified CRUD operation.
+     *
+     * @param namespace The namespace in the XML mapping files that corresponds to the CRUD operations to bind.
+     * @param id The ID of the CRUD tag within the namespace to bind.
+     * @param dBhandle The DB handle that encapsulates the CRUD operations and data source management.
+     *
+     * @return true if the binding was successful, false otherwise.
+     *<p>
+     * Description:
+     *   This method sets up the specified XML mapping namespace and CRUD tag ID to use the provided DB handle for the database qurey operation.
+     *<p>
+     * Example:
+     *<p> Assuming "com.example.mappers.users" is the namespace in the XML mapping files
+     *<p> And "getUserById" is the ID of the CRUD tag within the namespace
+     *<p>  And "dBhandle" is an instance of DBhandle configured for the database connection and CRUD operations
+     *<blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper("com.example.mappers.users", "getUserById", dBhandle);
+     *</pre></blockquote>
+     */
+    public static boolean bindMapper(String namespace, String id, DBhandle dBhandle) {
+        return defaultSlaveHandle.bindMapper(namespace, id, dBhandle);
+    }
+
+    public static void unbindMapper(String namespace, String id) {
+        defaultSlaveHandle.unbindMapper(namespace, id);
+    }
+
+    /**
+     * Binds the specified mapper interface to use the given DB handle for all methods within the interface.
+     *
+     * @param mapperface The mapper interface class that defines the methods to bind.
+     * @param dBhandle The DB handle that encapsulates the CRUD operations and data source management.
+     *
+     * @return true if the binding was successful, false otherwise.
+     *<p>
+     * Description:
+     *   This method sets up the specified mapper interface to use the provided DB handle for the database qurey operation.
+     *<p>
+     * Example:
+     *<p>  Assuming UserMapper is the mapper interface class
+     *<p>  And "dBhandle" is an instance of DBhandle configured for the database connection and CRUD operations
+     * <blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper(UserMapper.class, dBhandle);
+     *</pre></blockquote>
+     */
+    public static boolean bindMapper(Class<?> mapperface, DBhandle dBhandle) {
+        return defaultSlaveHandle.bindMapper(mapperface, dBhandle);
+    }
+
+    /**
+     * Binds the specified mapper interface to use the given data source and database type for all methods within the interface.
+     *
+     * @param mapperface The mapper interface class that defines the methods to bind.
+     * @param dataSource The data source to use for database connections.
+     * @param dbtype The type of the database (e.g., DBType.MYSQL, DBType.POSTGRESQL).
+     *
+     * @return true if the binding was successful, false otherwise.
+     *<p>
+     * Description:
+     *   This method sets up the specified mapper interface to use the provided data source and database type for the database qurey operation.
+     *<p>
+     * Example:
+     *<p> Assuming UserMapper is the mapper interface class
+     *<p> And "dataSource" is an instance of DataSource configured for the database connection
+     *<p> And "DBType.MYSQL" represents the type of the database
+     * <blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper(UserMapper.class, dataSource, DBType.MYSQL);
+     * </pre></blockquote>
+     */
+    public static boolean bindMapper(Class<?> mapperface, DataSource dataSource, DBType dbtype) {
+        return defaultSlaveHandle.bindMapper(mapperface, dataSource, dbtype);
+    }
+
+    public static void unbindMapper(Class<?> mapperface) {
+        defaultSlaveHandle.unbindMapper(mapperface);
+    }
+
+    /**
+     * Binds the specified XML mapping namespace to use the given data source and database type for all methods within the namespace.
+     *
+     * @param namespace The namespace in the XML mapping files that corresponds to the CRUD operations to bind.
+     * @param dataSource The data source to use for database connections.
+     * @param dbtype The type of the database (e.g., DBType.MYSQL, DBType.POSTGRESQL).
+     *
+     * @return true if the binding was successful, false otherwise.
+     *<p>
+     * Description:
+     *   This method sets up the specified XML mapping namespace to use the provided data source and database type for the database qurey operation.
+     *<p>
+     * Example:
+     * <p>  // Assuming "com.example.mappers.users" is the namespace in the XML mapping files
+     * <p>  // And "dataSource" is an instance of DataSource configured for the database connection
+     * <p>  // And "DBType.MYSQL" represents the type of the database
+     * <blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper("com.example.mappers.users", dataSource, DBType.MYSQL);
+     * </pre></blockquote>
+     */
+    public static boolean bindMapper(String namespace, DataSource dataSource, DBType dbtype) {
+        return defaultSlaveHandle.bindMapper(namespace, dataSource, dbtype);
+    }
+
+    /**
+     * Binds the specified XML mapping namespace to use the given DB handle for all methods within the namespace.
+     *
+     * @param namespace The namespace in the XML mapping files that corresponds to the CRUD operations to bind.
+     * @param dBhandle The DB handle that encapsulates the CRUD operations and data source management.
+     *
+     * @return true if the binding was successful, false otherwise.
+     *<p>
+     * Description:
+     *   This method sets up the specified XML mapping namespace to use the provided DB handle for all the database qurey operation.
+     *<p>
+     * Example:
+     *<p>   Assuming "com.example.mappers.users" is the namespace in the XML mapping files
+     *<p>   And "dBhandle" is an instance of DBhandle configured for the database connection and CRUD operations
+     * <blockquote><pre>
+     *   boolean bound = JdaoSlave.bindMapper("com.example.mappers.users", dBhandle);
+     * </pre></blockquote>
+     */
+    public static boolean bindMapper(String namespace, DBhandle dBhandle) {
+        return defaultSlaveHandle.bindMapper(namespace, dBhandle);
+    }
+
+    public static void unbindMapper(String namespace) {
+        defaultSlaveHandle.unbindMapper(namespace);
     }
 
     public static DBhandle get(Class<? extends Table<?>> clz, String packageName) {
-        return defaultSlaveHandler.get(clz, packageName);
+        return defaultSlaveHandle.get(clz, packageName);
     }
 
     public static DBhandle getMapper(String namespace, String id) {
-        return defaultSlaveHandler.getMapper(namespace, id);
+        return defaultSlaveHandle.getMapper(namespace, id);
     }
 
 }
