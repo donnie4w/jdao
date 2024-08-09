5c84349dff047148629a99e9e8b4c6681cea20ab
donnie4w <donnie4w@gmail.com>
2024-08-09 21:00:15 +0800

    Create FieldBean.java

---
+++ src/main/java/io/github/donnie4w/jdao/build/FieldBean.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2024, donnie <donnie4w@gmail.com> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * github.com/donnie4w/jdao
+ */
+
+package io.github.donnie4w.jdao.build;
+
+class FieldBean {
+    String fieldName;
+    int fieldIndex;
+    int fieldType;
+    String FieldTypeName;
+
+    public FieldBean(String fieldName, int fieldIndex, int fieldType, String FieldTypeName) {
+        this.fieldName = fieldName;
+        this.fieldIndex = fieldIndex;
+        this.fieldType = fieldType;
+        this.FieldTypeName = FieldTypeName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldTypeName() {
+        return FieldTypeName;
+    }
+
+    public void setFieldTypeName(String fieldTypeName) {
+        FieldTypeName = fieldTypeName;
+    }
+
+    public int getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(int fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public int getFieldIndex() {
+        return fieldIndex;
+    }
+
+    public void setFieldIndex(int fieldIndex) {
+        this.fieldIndex = fieldIndex;
+    }
+}
