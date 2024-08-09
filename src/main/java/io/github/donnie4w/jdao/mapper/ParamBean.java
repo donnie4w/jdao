2def739255f9c8f30f7e46af944b5e1068805f38
donnie4w <donnie4w@gmail.com>
2024-08-09 21:00:52 +0800

    Update ParamBean.java

--- src/main/java/io/github/donnie4w/jdao/mapper/ParamBean.java
+++ src/main/java/io/github/donnie4w/jdao/mapper/ParamBean.java
@@ -30,7 +30,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ParamBean {
+class ParamBean {
     private final String namespace;
     private final String id;
     private final SqlType sqlType;
