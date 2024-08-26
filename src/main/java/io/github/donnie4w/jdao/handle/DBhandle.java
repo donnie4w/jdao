8e1882ce59a57114840beb62cf8773a0e165ed43
donnie4w <donnie4w@gmail.com>
2024-08-26 16:53:33 +0800

    Update DBhandle.java

--- src/main/java/io/github/donnie4w/jdao/handle/DBhandle.java
+++ src/main/java/io/github/donnie4w/jdao/handle/DBhandle.java
@@ -1,5 +1,6 @@
 package io.github.donnie4w.jdao.handle;
 
+import io.github.donnie4w.jdao.base.JStruct;
 import io.github.donnie4w.jdao.base.Params;
 
 import java.sql.SQLException;
@@ -39,11 +40,11 @@ public interface DBhandle extends Basehandle {
      * the first if there are multiple query results
      *
      * @param transaction the transaction to use
-     * @param sql the SQL query to execute
-     * @param values the values to bind to the query
+     * @param sql         the SQL query to execute
+     * @param values      the values to bind to the query
      * @return the DataBean result
      * @throws JdaoException if there is an error executing the query
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException  if there is an error obtaining the transaction
      */
     DataBean executeQueryBean(Transaction transaction, String sql, Object... values) throws JdaoException, SQLException;
 
@@ -51,11 +52,11 @@ public interface DBhandle extends Basehandle {
      * executeQuery transactional operation
      *
      * @param transaction the transaction to use
-     * @param sql the SQL query to execute
-     * @param values the values to bind to the query
+     * @param sql         the SQL query to execute
+     * @param values      the values to bind to the query
      * @return the DataBean list result
      * @throws JdaoException if there is an error executing the query
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException  if there is an error obtaining the transaction
      */
     List<DataBean> executeQueryBeans(Transaction transaction, String sql, Object... values) throws JdaoException, SQLException;
 
@@ -64,14 +65,14 @@ public interface DBhandle extends Basehandle {
      * or all of them if there are multiple query results
      *
      * @param transaction the transaction to use
-     * @param claz the class of the objects to be created
-     * @param sql the SQL query to execute
-     * @param values the values to bind to the query
-     * @param <T> the type of the objects to be created
+     * @param claz        the class of the objects to be created
+     * @param sql         the SQL query to execute
+     * @param values      the values to bind to the query
+     * @param <T>         the type of the objects to be created
      * @return a list of results
-     * @throws JdaoException if there is an error executing the query
+     * @throws JdaoException      if there is an error executing the query
      * @throws JdaoClassException if there is an error with the class
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException       if there is an error obtaining the transaction
      */
     <T> List<T> executeQueryList(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException, JdaoClassException, SQLException;
 
@@ -80,15 +81,15 @@ public interface DBhandle extends Basehandle {
      * executeQuery Non-transactional operation and returns one piece of data,
      * the first if there are multiple query results
      *
-     * @param <T> the type of the object to be created
+     * @param <T>         the type of the object to be created
      * @param transaction the transaction to use
-     * @param claz the class of the object to be created
-     * @param sql the SQL query to execute
-     * @param values the values to bind to the query
+     * @param claz        the class of the object to be created
+     * @param sql         the SQL query to execute
+     * @param values      the values to bind to the query
      * @return the result
-     * @throws JdaoException if there is an error executing the query
+     * @throws JdaoException      if there is an error executing the query
      * @throws JdaoClassException if there is an error with the class
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException       if there is an error obtaining the transaction
      */
     <T> T executeQuery(Transaction transaction, Class<T> claz, String sql, Object... values) throws JdaoException, JdaoClassException, SQLException;
 
@@ -96,11 +97,11 @@ public interface DBhandle extends Basehandle {
      * executeUpdate transactional operation
      *
      * @param transaction the transaction to use
-     * @param sql the SQL update statement to execute
-     * @param values the values to bind to the update statement
+     * @param sql         the SQL update statement to execute
+     * @param values      the values to bind to the update statement
      * @return the number of rows affected
      * @throws JdaoException if there is an error executing the update
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException  if there is an error obtaining the transaction
      */
     int executeUpdate(Transaction transaction, String sql, Object... values) throws JdaoException, SQLException;
 
@@ -108,11 +109,11 @@ public interface DBhandle extends Basehandle {
      * Executes a batch of update statements.
      *
      * @param transaction the transaction to use
-     * @param sql the SQL update statement to execute
-     * @param values a list of value arrays for each update statement
+     * @param sql         the SQL update statement to execute
+     * @param values      a list of value arrays for each update statement
      * @return an array of update counts
      * @throws JdaoException if there is an error executing the updates
-     * @throws SQLException if there is an error obtaining the transaction
+     * @throws SQLException  if there is an error obtaining the transaction
      */
     int[] executeBatch(Transaction transaction, String sql, List<Object[]> values) throws JdaoException, SQLException;
 
@@ -120,12 +121,15 @@ public interface DBhandle extends Basehandle {
     /**
      * Executes a stored procedure call.
      *
-     * @param transaction the transaction to use
+     * @param transaction   the transaction to use
      * @param procedureName the name of the stored procedure to call
-     * @param params the parameters for the stored procedure
+     * @param params        the parameters for the stored procedure
      * @return a map of result set columns to their values
      * @throws SQLException if there is an error executing the procedure call
      */
     Map<Integer, Object> executeCall(Transaction transaction, String procedureName, Params... params) throws SQLException;
 
+    <T extends JStruct<T>> T newStruct(Class<T> clazz);
+
+    <T extends JStruct<T>> T newStruct(Class<T> clazz,String tablename);
 }
