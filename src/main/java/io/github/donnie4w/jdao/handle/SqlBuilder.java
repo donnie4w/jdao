584b947c252c20363721fd83465535095bdffa7c
donnie4w <donnie4w@gmail.com>
2024-09-09 09:17:45 +0800

    Update SqlBuilder.java

--- src/main/java/io/github/donnie4w/jdao/handle/SqlBuilder.java
+++ src/main/java/io/github/donnie4w/jdao/handle/SqlBuilder.java
@@ -18,267 +18,148 @@
 
 package io.github.donnie4w.jdao.handle;
 
-import io.github.donnie4w.jdao.util.RPNCalculator;
-import io.github.donnie4w.jdao.util.Utils;
-
-import java.lang.reflect.Array;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.function.Consumer;
 
-public class SqlBuilder {
-    private StringBuilder sql = new StringBuilder();
-    private final List<Object> parameters = new ArrayList<>();
+/**
+ * SqlBuilder is used for building dynamic SQL queries.
+ */
+public abstract class SqlBuilder {
 
-    public SqlBuilder append(String text) {
-        sql.append(" ").append(text).append(" ");
-        return this;
+    /**
+     * Sets the database handle to be used by the current SQL builder.
+     * This allows the builder to make appropriate adjustments based on different database handles.
+     *
+     * @param dBhandle The database handle to use
+     */
+    public abstract void useDBhandle(DBhandle dBhandle);
+
+    /**
+     * Appends a piece of text to the current SQL statement.
+     *
+     * @param text The string to append
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder append(String text);
+
+    /**
+     * Appends a piece of text with parameters to the current SQL statement.
+     *
+     * @param text The string to append
+     * @param params A variadic list of values that may be needed for subsequent parameters
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder append(String text, Object... params);
+
+    /**
+     * Conditionally appends a piece of text to the current SQL statement.
+     *
+     * @param expression The boolean expression string used to determine whether to append the text
+     * @param context The object used to evaluate the expression
+     * @param text The string to append if the condition is met
+     * @param params A variadic list of values that may be needed for subsequent parameters
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder appendIf(String expression, Object context, String text, Object... params);
+
+    /**
+     * Conditionally appends different parts to the current SQL statement based on choices.
+     *
+     * @param object The object used to evaluate the choice logic
+     * @param chooseBuilderConsumer A function that defines the content of different choice branches
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder appendChoose(Object object, Consumer<ChooseBuilder> chooseBuilderConsumer);
+
+    /**
+     * Appends a loop structure to the current SQL statement.
+     *
+     * @param collectionName The name of the variable used for iterating over a collection
+     * @param context The object used to evaluate the loop logic
+     * @param item The name of the variable representing each element within the loop
+     * @param separator The text between loop iterations
+     * @param open The opening text of the loop body
+     * @param close The closing text of the loop body
+     * @param foreachConsumer A function that defines the content of the loop body
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder appendForeach(String collectionName, Object context, String item, String separator, String open, String close, Consumer<ForeachBuilder> foreachConsumer);
+
+    /**
+     * Appends a trimming structure to the current SQL statement to remove extra prefixes or suffixes.
+     *
+     * @param prefix The trimming prefix text
+     * @param suffix The trimming suffix text
+     * @param prefixOverrides The text used to override the prefix
+     * @param suffixOverrides The text used to override the suffix
+     * @param contentBuilder A function that defines the trimming content
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder appendTrim(String prefix, String suffix, String prefixOverrides, String suffixOverrides, Consumer<SqlBuilder> contentBuilder);
+
+    /**
+     * Appends a SET clause to the current SQL statement.
+     *
+     * @param contentBuilder A function that defines the content of the SET clause
+     * @return The SqlBuilder instance itself, supporting method chaining
+     */
+    public abstract SqlBuilder appendSet(Consumer<SqlBuilder> contentBuilder);
+
+    /**
+     * Retrieves the final constructed SQL statement.
+     *
+     * @return A string representing the complete SQL statement
+     */
+    public abstract String getSql();
+
+    /**
+     * Retrieves all collected parameters during the construction process.
+     *
+     * @return An array of objects containing all parameters
+     */
+    public abstract Object[] getParameters();
+
+    /**
+     * Creates a new SqlBuilder instance.
+     *
+     * @return A new SqlBuilder instance
+     */
+    public static SqlBuilder newInstance() {
+        return new SqlBuildHandler();
     }
 
-    public SqlBuilder append(String text, Object... params) {
-        sql.append(" ").append(text).append(" ");
-        addParameters(params);
-        return this;
-    }
+    /**
+     * Adds parameters to the current SQL builder.
+     *
+     * @param params The parameters to add
+     */
+    protected abstract void addParameters(Object... params);
 
-    public SqlBuilder appendIf(String expression, Object context, String text, Object... params) {
-        if (RPNCalculator.evaluate(expression, Utils.toMap(context))) {
-            sql.append(" ").append(text);
-            addParameters(params);
-        }
-        return this;
-    }
+    /**
+     * Executes a SQL query and returns the first record.
+     *
+     * @return The first record of the query result
+     * @throws JdaoException If a Jdao exception occurs during execution
+     * @throws SQLException If a SQL exception occurs during execution
+     */
+    public abstract DataBean selectOne() throws JdaoException, SQLException;
 
-    public SqlBuilder appendIf(String expression, Map<String, Object> context, String text, Object... params) {
-        if (RPNCalculator.evaluate(expression, context)) {
-            sql.append(" ").append(text);
-            addParameters(params);
-        }
-        return this;
-    }
+    /**
+     * Executes a SQL query and returns all records.
+     *
+     * @return A list of all records of the query result
+     * @throws JdaoException If a Jdao exception occurs during execution
+     * @throws SQLException If a SQL exception occurs during execution
+     */
+    public abstract List<DataBean> selectList() throws JdaoException, SQLException;
 
-    public SqlBuilder appendChoose(Object object, Consumer<ChooseBuilder> chooseBuilderConsumer) {
-        ChooseBuilder chooseBuilder = new ChooseBuilder(this, Utils.toMap(object));
-        chooseBuilderConsumer.accept(chooseBuilder);
-        return this;
-    }
-
-    public SqlBuilder appendChoose(Map<String, Object> context, Consumer<ChooseBuilder> chooseBuilderConsumer) {
-        ChooseBuilder chooseBuilder = new ChooseBuilder(this, context);
-        chooseBuilderConsumer.accept(chooseBuilder);
-        return this;
-    }
-
-    public SqlBuilder appendForeach(String collectionName, Object context, String item, String separator, String open, String close, Consumer<ForeachBuilder> foreachConsumer) {
-        return appendForeach(collectionName, Utils.toMap(context), item, separator, open, close, foreachConsumer);
-    }
-
-    public SqlBuilder appendForeach(String collectionName, Map<String, Object> context, String item, String separator, String open, String close, Consumer<ForeachBuilder> foreachConsumer) {
-        Object collectionObj = context.get(collectionName);
-        if (collectionObj == null) {
-            return this;
-        }
-        Collection<?> collection = null;
-        if (collectionObj.getClass().isArray()) {
-            int length = Array.getLength(collectionObj);
-            List<Object> list = new ArrayList<>();
-            for (int i = 0; i < length; i++) {
-                list.add(Array.get(collectionObj, i));
-            }
-            collection = list;
-        } else if (collectionObj instanceof Collection<?>) {
-            collection = (Collection<?>) collectionObj;
-        }
-        if (collection != null && !collection.isEmpty()) {
-            if (open != null) {
-                sql.append(open);
-            }
-            ForeachBuilder foreachBuilder = new ForeachBuilder(this, separator);
-            foreachConsumer.accept(foreachBuilder);
-
-            Iterator<?> iterator = collection.iterator();
-            while (iterator.hasNext()) {
-                Object currentItem = iterator.next();
-                String replacedBody = foreachBuilder.getBody().replace("#{" + item + "}", "?");
-                sql.append(replacedBody);
-                addParameter(currentItem);
-                if (separator != null && iterator.hasNext()) {
-                    sql.append(separator);
-                }
-            }
-            if (close != null) {
-                sql.append(close);
-            }
-        }
-
-        return this;
-    }
-
-
-    public SqlBuilder appendTrim(String prefix, String suffix, String prefixOverrides, String suffixOverrides, Consumer<SqlBuilder> contentBuilder) {
-        StringBuilder tempSql = new StringBuilder();
-        SqlBuilder tempBuilder = new SqlBuilder(tempSql);
-        contentBuilder.accept(tempBuilder);
-        while (tempSql.length() > 0 && Character.isWhitespace(tempSql.charAt(0))) {
-            tempSql.deleteCharAt(0);
-        }
-        while (tempSql.length() > 0 && Character.isWhitespace(tempSql.charAt(tempSql.length() - 1))) {
-            tempSql.deleteCharAt(tempSql.length() - 1);
-        }
-        if (prefixOverrides != null) {
-            String[] prefixes = prefixOverrides.split("\\|");
-            for (String override : prefixes) {
-                if (tempSql.indexOf(override) == 0) {
-                    tempSql.delete(0, override.length());
-                    break;
-                }
-            }
-        }
-        if (suffixOverrides != null) {
-            String[] suffixes = suffixOverrides.split("\\|");
-            for (String override : suffixes) {
-                if (tempSql.lastIndexOf(override) == tempSql.length() - override.length()) {
-                    tempSql.delete(tempSql.length() - override.length(), tempSql.length());
-                }
-            }
-        }
-
-        if (tempSql.length() > 0) {
-            if (Utils.stringValid(prefix)) {
-                tempSql.insert(0, prefix);
-            }
-            if (Utils.stringValid(suffix)) {
-                tempSql.append(suffix);
-            }
-            tempSql.append(' ');
-            sql.append(tempSql);
-        }
-        parameters.addAll(tempBuilder.parameters);
-        return this;
-    }
-
-
-    public String getSql() {
-        return sql.toString();
-    }
-
-    public Object[] getParameters() {
-        return parameters.toArray();
-    }
-
-    private void addParameter(Object param) {
-        parameters.add(param);
-    }
-
-    private void addParameters(Object... params) {
-        parameters.addAll(Arrays.asList(params));
-    }
-
-    public static class ChooseBuilder {
-        private final SqlBuilder parentBuilder;
-        private final Map<String, Object> context;
-        private boolean conditionMet = false;
-
-        public ChooseBuilder(SqlBuilder parentBuilder, Map<String, Object> context) {
-            this.parentBuilder = parentBuilder;
-            this.context = context;
-        }
-
-        public ChooseBuilder when(String expression, String sql, Object... params) {
-            if (!conditionMet && RPNCalculator.evaluate(expression, context)) {
-                parentBuilder.append(sql).addParameters(params);
-                conditionMet = true;
-            }
-            return this;
-        }
-
-        public ChooseBuilder otherwise(String sql, Object... params) {
-            if (!conditionMet) {
-                parentBuilder.append(sql).addParameters(params);
-            }
-            return this;
-        }
-    }
-
-    public static class ForeachBuilder {
-        private final SqlBuilder parentBuilder;
-        private final String separator;
-        private String body;
-
-        public ForeachBuilder(SqlBuilder parentBuilder, String separator) {
-            this.parentBuilder = parentBuilder;
-            this.separator = separator;
-        }
-
-        public ForeachBuilder body(String body) {
-            this.body = body;
-            return this;
-        }
-
-        public String getBody() {
-            return body;
-        }
-    }
-
-    public SqlBuilder appendSet(Consumer<SqlBuilder> contentBuilder) {
-        StringBuilder tempSql = new StringBuilder();
-        SqlBuilder tempBuilder = new SqlBuilder(tempSql);
-        contentBuilder.accept(tempBuilder);
-        int length = tempSql.length();
-        while (length > 0 && (tempSql.charAt(length - 1) == ',' || Character.isWhitespace(tempSql.charAt(length - 1)))) {
-            tempSql.deleteCharAt(length - 1);
-            length--;
-        }
-        if (length > 0) {
-            sql.append("SET ").append(tempSql).append(" ");
-        }
-        this.parameters.addAll(tempBuilder.parameters);
-        return this;
-    }
-
-
-    private SqlBuilder(StringBuilder tempSql) {
-        this.sql = tempSql;
-    }
-
-    public SqlBuilder() {
-    }
-
-    public static void main(String[] args) {
-        SqlBuilder builder = new SqlBuilder();
-        Map<String, Object> context = new HashMap<>();
-        context.put("username", "John Doe");
-        context.put("age", 30);
-        context.put("emails", Arrays.asList("john@example.com", "doe@example.com", "john.doe@example.com"));
-
-        builder.append("SELECT * FROM users")
-                .appendTrim("WHERE", "", "AND", "", trimBuilder -> {
-                    trimBuilder.appendIf("username != null", context, "AND username = ?", context.get("username"))
-                            .appendIf("age > 18", context, "AND age = ?", context.get("age"))
-                            .appendChoose(context, choose -> choose
-                                    .when(" age > 10", "AND age = ?", context.get("age"))
-                                    .when("username!=null", "AND username = ?", context.get("username"))
-                                    .otherwise("email IS NULL")
-                            );
-                })
-                .append("AND email in")
-                .appendForeach("emails", context, "email", ",", "(", ")", foreachBuilder -> foreachBuilder
-                        .body("#{email}")
-                )
-                .append("ORDER BY id ASC");
-        String sql = builder.getSql();
-        System.out.println(sql);
-        System.out.println(builder.parameters);
-        System.out.println("-------------------------------------------------------------------");
-        builder = new SqlBuilder()
-                .append("UPDATE users ")
-                .appendSet(set -> set
-                        .appendIf("username != null", context, "username = ?,", context.get("username"))
-                        .appendIf("age == 30", context, "age = ?,", context.get("age"))
-                )
-                .append("WHERE id = ?", 10);
-
-        System.out.println(sql);
-        System.out.println(builder.parameters);
-    }
-
+    /**
+     * Executes a SQL statement and returns the number of affected rows.
+     *
+     * @return The number of affected rows
+     * @throws JdaoException If a Jdao exception occurs during execution
+     * @throws SQLException If a SQL exception occurs during execution
+     */
+    public abstract int exec() throws JdaoException, SQLException;
 }
